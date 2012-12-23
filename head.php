fd6eb3d80c9535468e6c1df4680af1139e385574
Pierre Rudloff <contact@rudloff.pro>
2012-12-23 14:54:25 +0000

    script async

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8" />
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
-<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+<script async src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+<script async src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 <link rel="icon" href="favicon.png" type="image/svg+xml" />
 </head>
     <body>
