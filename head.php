3d69457f4f6256caa69ae907a906db056c33d09e
Pierre Rudloff <contact@rudloff.pro>
2012-12-27 21:10:40 +0000

    Viewport

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 <script async src="jquery-1.8.2.min.js"></script>
 <script async src="jquery.mobile-1.2.0.min.js"></script>
 <link rel="icon" href="favicon.png" type="image/svg+xml" />
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
 </head>
     <body>
         <div data-role="page">
