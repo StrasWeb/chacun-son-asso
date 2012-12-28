2c16de60efb704144a46bff0453e21ad12216ea0
Pierre Rudloff <contact@rudloff.pro>
2012-12-28 01:58:22 +0000

    Problème avec jQuery Mobile en async

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8" />
 <link rel="stylesheet" href="jquery.mobile-1.2.0.min.css" />
-<script async src="jquery-1.8.2.min.js"></script>
-<script async src="jquery.mobile-1.2.0.min.js"></script>
+<script src="jquery-1.8.2.min.js"></script>
+<script src="jquery.mobile-1.2.0.min.js"></script>
 <link rel="icon" href="favicon.png" type="image/svg+xml" />
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 </head>
